887a802759b67205176d89e3256e3d8273475b96
Antoine B <aberthillot@myges.fr>
2026-03-05 16:31:28 +0100

    dockerization

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 4173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://server:3000",
         changeOrigin: true,
       },
     },
--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -2,10 +2,11 @@ import { DataSource } from "typeorm";
 
 const datasource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "supersecret",
-  database: "postgres",
+  host: process.env.DB_HOST || "db",
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "supersecret",
+  database: process.env.DB_NAME || "postgres",
   entities: [__dirname + "/entities/**/*.{js,ts}"],
   logging: true,
   synchronize: true,
